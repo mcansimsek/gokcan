1499a618090c614524fe791f7ad7335fa9802177
Mustafa Can Simsek <mustafa.can.simsek@sap.com>
2025-03-24 23:40:16 +0100

    bug fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    darkMode: 'class',
-    theme: {
-      extend: {
-        colors: {
-          primary: '#1a202c',
-          secondary: '#4a5568',
-          accent: '#2b6cb0',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1a202c',
+        secondary: '#4a5568',
+        accent: '#2b6cb0',
+        stone: {
+          800: '#44403c',
+          700: '#57534e',
+        },
+        slate: {
+          100: '#f1f5f9',
+          200: '#e2e8f0',
+          700: '#334155',
+          800: '#1e293b',
+          900: '#0f172a',
         },
       },
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+};
